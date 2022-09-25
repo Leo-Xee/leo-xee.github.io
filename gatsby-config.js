12c263fe832a600b53ae2657e2edc15c4ff425d1
leo-xee <jang.xmin@gmail.com>
2022-09-26 00:07:41 +0900

    fix: 오타 수정

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,7 +68,7 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-google-gtag`,
+      resolve: `gatsby-plugin-gtag`,
       options: {
         trackingId: metaConfig.ga,
         head: true,
